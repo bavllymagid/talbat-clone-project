3b92101ee9aacdab122f67b654790853b243d2cc
Anas-Abd-ElAziz <mohamedanas709@gmail.com>
2020-12-21 20:28:12 +0200

    MEALS and RESTURANT EDITS

--- Talabat/app/src/main/java/project/java4/talabat/Meal.java
+++ Talabat/app/src/main/java/project/java4/talabat/Meal.java
@@ -1,7 +1,37 @@
 package project.java4.talabat;
 
 public class Meal {
-    float prise;
-    String name;
-    String Description;
+
+    private int imageResource ;
+    private float price ;
+    private String name ;
+    private String description ;
+    private String Number;
+    Meal(String name, float price, String description, int imageResource, String Number){
+        this.name = name ;
+        this.price=price ;
+        this.description= description ;
+        this.imageResource = imageResource ;
+        this.Number=Number;
+    }
+
+    public void setImageResource(int imageResource) {
+        this.imageResource = imageResource;
+    }
+
+    public void setPrice(float price) {
+        this.price = price;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getName() {
+        return name;
+    }
 }
