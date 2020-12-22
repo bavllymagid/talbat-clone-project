008d6ead6bb03405450912cc7e6ff19dbd82607c
Anas-Abd-ElAziz <mohamedanas709@gmail.com>
2020-12-22 19:19:21 +0200

    Added meal list template with meal adaptor and edited meal class

--- Talabat/app/src/main/java/project/java4/talabat/Meal.java
+++ Talabat/app/src/main/java/project/java4/talabat/Meal.java
@@ -1,25 +1,39 @@
 package project.java4.talabat;
-
 public class Meal {
 
     private int imageResource ;
-    private float price ;
+    private String price ;
     private String name ;
     private String description ;
     private String Number;
-    Meal(String name, float price, String description, int imageResource, String Number){
+    Meal(int imageResource,String name, String price, String description,String Number){
+        this.imageResource=imageResource;
         this.name = name ;
         this.price=price ;
         this.description= description ;
-        this.imageResource = imageResource ;
         this.Number=Number;
+    }
+    public int getImageResource() {
+        return imageResource;
+    }
+
+    public String getPrice() {
+        return price;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDescription() {
+        return description;
     }
 
     public void setImageResource(int imageResource) {
         this.imageResource = imageResource;
     }
 
-    public void setPrice(float price) {
+    public void setPrice(String price) {
         this.price = price;
     }
 
@@ -30,8 +44,5 @@ public class Meal {
     public void setDescription(String description) {
         this.description = description;
     }
-
-    public String getName() {
-        return name;
-    }
 }
+
